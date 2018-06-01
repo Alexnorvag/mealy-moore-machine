850eb5353a7f82d3da8e3512e950b2b0357a8ca7
Alexnorvag <norvag@mail.ru>
2018-06-01 20:59:40 +0300

    changed date output format

--- src/Commons/Header.js
+++ src/Commons/Header.js
@@ -19,32 +19,40 @@ class Header extends Component {
     }
 
     render() {
-        // console.log("DATE NOW: ", new Date().toLocaleString());
         const {currentTime} = this.state;
 
         return (
             <div className="header">
                 <nav className="navbar navbar-light bg-light">
-                    <a className="navbar-brand text-uppercase" href="/home">Home</a>
-                    <div className="">{(currentTime)}</div>
-
-                    <div className="btn-group">
-                        <button type="button" className="btn btn-primary">Sasha Ignatenya</button>
-                        <button
-                            type="button"
-                            className="btn btn-primary dropdown-toggle dropdown-toggle-split"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false">
-                            <span className="sr-only">Toggle Dropdown</span>
-                        </button>
-                        <div className="dropdown-menu">
-                            <a className="dropdown-item" href="/profile">Profile</a>
-                            <a className="dropdown-item" href="/settings">Settings</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="/logout">Log out</a>
+                    {/* <div className="row"> */}
+                        <div className="col-4">
+                            <a className="navbar-brand text-uppercase" href="/home">Home</a>
                         </div>
-                    </div>
+                        <div className="col-4 d-inline text-center">
+                            <div className="mb-1">{(currentTime.substring(0, currentTime.indexOf(', ')))}</div>
+                            <div className="">{(currentTime.substring(currentTime.indexOf(', ') + 2))}</div>
+                        </div>
+
+                        <div className="col-4 d-flex justify-content-end">
+                            <div className="btn-group">
+                                <button type="button" className="btn btn-primary">Sasha Ignatenya</button>
+                                <button
+                                    type="button"
+                                    className="btn btn-primary dropdown-toggle dropdown-toggle-split"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false">
+                                    <span className="sr-only">Toggle Dropdown</span>
+                                </button>
+                                <div className="dropdown-menu">
+                                    <a className="dropdown-item" href="/profile">Profile</a>
+                                    <a className="dropdown-item" href="/settings">Settings</a>
+                                    <div className="dropdown-divider"></div>
+                                    <a className="dropdown-item" href="/logout">Log out</a>
+                                </div>
+                            </div>
+                        </div>
+                    {/* </div> */}
 
                 </nav>
             </div>
